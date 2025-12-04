2931f7aea30270f22621d8f74d13f0cbc4b8d3e1
ApexOrigin <travisferns23@gmail.com>
2025-12-04 22:47:03 +0530

    Initial project setup with Django settings and views structure

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,16 @@ SECRET_KEY = 'django-insecure-*y4r7^p_9f_2dm0a%-x@jzk9jc6ukk)jck&z=@+66q_9^r4nrq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+
+    ".railway.app" # https://saas.prod.railway.app
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -21,14 +21,24 @@ def my_old_home_page_view(request, *args, **kwargs):
     # html_file = html
     return HttpResponse ("<h1>Welcome to the CFE Home Page!</h1>")
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+   return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):      
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    
+    try:
+        percent = page_qs.count() / qs.count() * 100
+    except :
+        percent = 0
     my_title = "My Page"
     my_context = {
         "page_title": my_title
         ,"page_visit_count":page_qs.count(),
-        "percent": page_qs.count() / qs.count() * 100 if qs.count() > 0 else 0
+         "percent": (page_qs.count()* 100.0) / qs.count(),
+        "total_visit_count": qs.count(),
         }
     path = request.path
     print ("Path:", path)
