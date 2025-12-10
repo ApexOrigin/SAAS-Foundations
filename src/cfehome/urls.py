0b9460d8ae59cdc5a0e8e3e0278d51e7ebb1ca01
ApexOrigin <travisferns23@gmail.com>
2025-12-10 15:14:15 +0530

    Fix urls.py imports and clean up invalid module references

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -15,13 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import home_view,about_view
+from django.urls import path, include
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view,
+    staff_only_view
+)
 
 urlpatterns = [
-    path("", home_view),
-    path("about/" , about_view),
+    path("", about_view, name='home'),
+    path("about/", about_view, name='about'),
+    path("hello-world/", home_view, name='hello-world'),
     path("hello-world.html", home_view),
-    path("hello-world/", home_view),
-    path('admin/', admin.site.urls),
+    path('protected/user-only/', user_only_view),
+    path('protected/staff-only/', staff_only_view),
+    path('protected/', pw_protected_view),
+    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
 ]
